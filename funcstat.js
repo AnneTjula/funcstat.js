8c75d9389c6d7119f6bcbd0d0aef062e41f87946
AnneTjula <88149819+AnneTjula@users.noreply.github.com>
2021-08-31 15:29:56 +0200

    Add files via upload

---
+++ funcstat.js
@@ -0,0 +1,63 @@
+// eerste poging eerste deel opdracht returnstatements :
+
+/* const check = function (numbers) {
+    console.log (amount)
+    const amount = numbers > 100
+    const number= 110
+    };
+check (110); */
+
+
+//voorbeeld vorige opdracht: 
+
+/* const paintWall2 = function(color) {
+    console.log(`The wall has been painted ` + color); // String concatenation
+    // or
+    console.log(`The wall has been painted ${color}`); // String interpolation */
+
+//tweede poging:
+
+/* const check = function (amount, cm100) {
+    const totalAmount = cm120
+    console.log(amount + "must be bigger than" + cm100);
+        ;
+
+}
+check( cm120 ); */
+
+//derde poging
+/* const check = function (amount) {
+    console.log(amount + "is bigger than 100");
+    console.log(amount + "is equal to or less than 100")
+        ;
+
+    if (amount > 100)
+        return amount;
+    
+    else (amount <= 100)
+        return amount;
+        
+}
+check(110)
+check(50) */
+/*check (130)
+check (120)
+check (100)  */
+
+// bouncer at a club
+/* const establish = function (_age, _people, _currentAmount) { console.log("come in", "it's too busy now, come back later", "it's for adults"); }
+if (age < 18)
+    return "This is a club for adults."
+if (clubIsFull)
+    return "It's too busy now, come back later."
+if (age > 18 + clubIsNotFull)
+    return "Come in."
+     */
+
+// establish (age);
+
+//calculating the average
+const calculate = function (_average, _numbers) { console.log("calculate the" + average + "of five numbers"); }
+
+numbers = (10, 12, 18, 8, 9)
+return average;
